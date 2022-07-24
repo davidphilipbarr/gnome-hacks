833a672257d7b450dfa3c022fbb4d69dc0517892
David Barr <dpbarr@gmail.com>
2022-07-24 21:42:31 +0100

    smaller edge colours

--- altwaita/build/colourme.sh
+++ altwaita/build/colourme.sh
@@ -1,11 +1,13 @@
-PRICOLOR="#93a1a1"
-SECCOLOR="#586e75"
-TERCOLOR="#839496"
+PRICOLOR="#2aa198"
+SECCOLOR="#168078"
+TERCOLOR="#2aa198"
+EDGE="#06665f"
 
 echo $PRICOLOR
 echo $SECCOLOR
+echo $EDGE
 
-sed -e "s/pricolor/"$PRICOLOR"/g" -e "s/seccolor/"$SECCOLOR"/g" -e "s/tercolor/$TERCOLOR/g" folders.svg > ../src/fullcolor/folders.svg
+sed -e "s/pricolor/"$PRICOLOR"/g" -e "s/seccolor/"$SECCOLOR"/g" -e "s/tercolor/$TERCOLOR/g" -e "s/EDGED/"$EDGE"/g" -e "s/EDGEL/"$SECCOLOR"/g" folders.svg > ../src/fullcolor/folders.svg
 
 
 
